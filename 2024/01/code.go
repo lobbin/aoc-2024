ddd57be909551723c023ff721ba71c9f56cdd2cb
Robin Helgelin <lobbin@localhost.nu>
2024-12-01 09:41:01 +0100

    2024/01 - part 1 & 2

--- 2024/01/code.go
+++ 2024/01/code.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -8,17 +13,41 @@ func main() {
 	aoc.Harness(run)
 }
 
-// on code change, run will be executed 4 times:
-// 1. with: false (part1), and example input
-// 2. with: true (part2), and example input
-// 3. with: false (part1), and user input
-// 4. with: true (part2), and user input
-// the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
-	// when you're ready to do part 2, remove this "not implemented" block
-	if part2 {
-		return "not implemented"
+	// Parse file by looping lines, splitting by white space, converting the
+	// string to number and adding them to the lists
+	list1, list2 := make([]int, 0), make([]int, 0)
+	for _, line := range strings.Split(input, "\n") {
+		numbers := strings.Split(line, " ")
+		number1, _ := strconv.Atoi(strings.TrimSpace(numbers[0]))
+		number2, _ := strconv.Atoi(strings.TrimSpace(numbers[len(numbers)-1]))
+		list1 = append(list1, number1)
+		list2 = append(list2, number2)
 	}
-	// solve part 1 here
-	return 42
+
+	result := 0
+	if part2 {
+		// Pre-count all the numbers in second list
+		countMap := make(map[int]int)
+		for _, number := range list2 {
+			countMap[number]++
+		}
+
+		// Iterator first list and calculate using pre-counted values
+		for _, number := range list1 {
+			result += number * countMap[number]
+		}
+	} else {
+		// Sort lists ascending
+		slices.SortFunc(list1, func(i, j int) int { return i - j })
+		slices.SortFunc(list2, func(i, j int) int { return i - j })
+
+		// Loop the lists and add the diffrence to the reslut
+		for i := 0; i < len(list1); i++ {
+			result += int(math.Abs(float64(list1[i]) - float64(list2[i])))
+		}
+	}
+
+	// Return result
+	return result
 }
